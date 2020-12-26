f0fdd09f799f736bbc381a7d0e53bf430e4311c6
lekuid <business.lekuid@gmail.com>
2020-12-27 00:05:03 +0630

    lil update

--- BS4WhenJoinedCodeWars.py
+++ BS4WhenJoinedCodeWars.py
@@ -2,11 +2,14 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 def member_since(username):
-    req = requests.get(f'https://www.codewars.com/users/{username}')
-    page = bs(req.text, 'html.parser')
-    year = page.find_all('div', class_='stat-box mt-1 mb-1 md:mb-0')[1]
-    month = year.find('div').text.split(':')[-1]
-    return month
+    try:
+        req = requests.get(f'https://www.codewars.com/users/{username}')
+        page = bs(req.text, 'html.parser')
+        year = page.find_all('div', class_='stat-box mt-1 mb-1 md:mb-0')[1]
+        month = year.find('div').text.split(':')[-1]
+        return month
+    except:
+        return 'There is no such user on CodeWars!'
 
 
-print(member_since('tim'))
+print(member_since('lekuid'))
